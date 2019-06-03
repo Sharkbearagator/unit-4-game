8a4978babe4f3ce206b23d7707b5b7ff3b543d5a
Tyler Nichols <tegulator98@gmail.com>
2019-06-02 21:56:22 -0600

    Added images to the HTML and psuedo code to JS.

---
+++ javascript/game.js
@@ -0,0 +1,15 @@
+//Declare global variables
+
+//Create a random number generator
+
+//Display the random number
+
+//make a wins and loss if statements
+
+//create a random number generator in a for loop looping four times
+
+//assign those numbers to the crystals
+
+//make an on click event that adds those values to their score
+
+//display their score
